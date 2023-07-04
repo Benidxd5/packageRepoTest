6f8942e6ced4a5814c17eeea87e5c3b695a26ff3
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-04 14:34:41 +0200

    t14

--- 01-generate-manifests.ps1
+++ 01-generate-manifests.ps1
@@ -18,14 +18,14 @@ Get-ChildItem .\packages -Recurse -Include '*.yaml' | % {
     Remove-Item $_
 }
 
-Get-ChildItem .tmp -Recurse -Include '*.json' | % {
-    "Dirname"
-    $_.Directoryname
-    ([System.IO.Path]::GetFileNameWithoutExtension("$_") + ".yaml")
-    $item = (Get-Content -Path "$_" | ConvertFrom-Json)
-    Set-Content -Path (Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_") + ".yaml")) -Value ($item | ConvertTo-Yaml).TrimEnd()
-    Remove-Item $_
-}
+# Get-ChildItem .tmp -Recurse -Include '*.json' | % {
+#     "Dirname"
+#     $_.Directoryname
+#     ([System.IO.Path]::GetFileNameWithoutExtension("$_") + ".yaml")
+#     $item = (Get-Content -Path "$_" | ConvertFrom-Json)
+#     Set-Content -Path (Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_") + ".yaml")) -Value ($item | ConvertTo-Yaml).TrimEnd()
+#     Remove-Item $_
+# }
 # "TO YAML"
 # Get-ChildItem .\packages -Recurse -Include '*.yaml' | % {
 #     "YAML FOUND"
