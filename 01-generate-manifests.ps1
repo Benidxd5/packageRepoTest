416e202f1a5cc41f5b6141ee2ac577164ea488c3
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-07 10:02:28 +0200

    genmant12

--- 01-generate-manifests.ps1
+++ 01-generate-manifests.ps1
@@ -47,60 +47,107 @@
 
 
 Get-ChildItem .\packages -Recurse -Include '*.yaml' | % {
+    #if( $_ -like "*installer.yaml"){
     "Processing File"
     $_.Directoryname
 
     Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_")+".def.yaml")
 
     # Array mit Pfaden zu den YAML-Dateien
+    $inhalt = Get-Content -Path $_
+    $inhalt = $inhalt | ConvertFrom-Yaml
+    $inhalt["PackageIdentifier"]
     $quelleDateien = @(
         (Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_")+".yaml")),
-        (Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_")+".def.yaml"))
+        (Join-Path -Path $_.Directoryname -ChildPath ( $inhalt["PackageIdentifier"]+".def.yaml"))
     )
-
     "afterpaths"
     # Leeres Hashtable zum Speichern der Attribute
     $attributeHash = @{}
-
+    $combInh = ""
     # Schleife zum Durchlaufen der YAML-Dateien
     foreach ($datei in $quelleDateien) {
         # Inhalte der Datei lesen
         if (Test-Path $datei) {
-            $inhalt = Get-Content -Path $datei -Raw
+            #if( $datei -notlike "*def.yaml"){
+                "new file"
+                $inhalt = Get-Content -Path $datei -Raw
+                $datei
+                $inhalt
+                # Konvertiere den YAML-Inhalt zu einem Hashtable
+                $yamlInhalt = $inhalt | ConvertFrom-Yaml
+                "inh"
+                $yamlInhalt
+                # Schleife durch die Attribute und füge sie dem Hashtable hinzu
+                foreach ($attribut in $yamlInhalt) {
+                    $attributeHash[$attribut] = $attribut.Value
+                }
 
-            # Konvertiere den YAML-Inhalt zu einem Hashtable
-            $yamlInhalt = $inhalt | ConvertFrom-Yaml
-
-            # Schleife durch die Attribute und füge sie dem Hashtable hinzu
-            foreach ($attribut in $yamlInhalt.PSObject.Properties) {
-                $attributeHash[$attribut.Name] = $attribut.Value
-            }
-
-            "dateifound"
-        }
-        
+                "dateifound"
+                $attributeHash 
+                $combInh = $combInh+$inhalt
+                "combinh"
+                $combInh
+            #}
+        }   
     }
 
     # Konvertiere das Hashtable zurück in ein PowerShell-Objekt
     $kombiniertesObjekt = [PSCustomObject]$attributeHash
-
+    "komb"
     # Konvertiere das kombinierte Objekt zu YAML
-    $kombiniertesYaml = $kombiniertesObjekt | ConvertTo-Yaml 
-
+    $kombiniertesYaml = ($attributeHash | ConvertTo-Yaml)
     # Pfad zur Zieldatei
+    $kombiniertesYaml
     $zielDatei = $_.Directoryname+"\HS2N.PAKETTEST.def.yaml"
 
     # Speichere das kombinierte YAML in der Zieldatei
-    Set-Content -Path $zielDatei -Value $kombiniertesYaml
+    Set-Content -Path $zielDatei -Value $combInh
+
+
+    "yaml"
+    "content"
+    Get-Content -Path $zielDatei -Raw
 
     Write-Host "Die kombinierte Datei wurde erstellt: $zielDatei"
+   # }
 }
 
 
 
+# Installieren des PowerShell-Moduls 'powershell-yaml'
+# Beachte, dass du dies vor dem Ausf端hren des Skripts einmalig ausf端hren musst.
+# Dateipfade der Eingabedateien
+# Installieren des PowerShell-Moduls 'powershell-yaml'
+# Beachte, dass du dies vor dem Ausf端hren des Skripts einmalig ausf端hren musst.
 
 
+# Get-ChildItem .\packages -Recurse -Include '*.yaml' | % {
 
+# # Dateipfade der Eingabedateien
+# $file1 = (Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_")+".yaml")),
+# $file2 = (Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_")+".def.yaml"))
+
+# # Einlesen der Daten aus den Eingabedateien
+# $data1 = Get-Content -Raw -Path $file1 | ConvertFrom-Yaml
+# $data2 = Get-Content -Raw -Path $file2 | ConvertFrom-Yaml
+
+# # Zusammenf端hren der Daten
+# $mergedData = @{}
+# $mergedData += $data1
+# $mergedData += $data2
+
+
+# # Konvertieren der Daten in YAML
+# $yamlData = $mergedData | ConvertTo-Yaml -Depth 100
+
+# # Speichern der YAML-Daten in einer Datei
+# $outputFile = $_.Directoryname+"\HS2N.PAKETTEST.def.yaml"
+# $yamlData | Out-File -FilePath $outputFile -Encoding UTF8
+
+# Write-Host "Die Zusammenf端hrung wurde abgeschlossen und die Ausgabedatei wurde unter $outputFile gespeichert."
+
+# }
 
 
 
