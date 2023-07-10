fa58ea0146025568396c13fd590e165289e6f235
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-10 12:06:11 +0200

    genmant1

--- 01-generate-manifests.ps1
+++ 01-generate-manifests.ps1
@@ -1,7 +1,6 @@
 
 Get-ChildItem .\packages -Recurse -Include '*.yaml' | % {
     "Processing File"
-    $_
     $_.Directoryname
 
     Join-Path -Path $_.Directoryname -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension("$_")+".def.yaml")
@@ -16,41 +15,25 @@ Get-ChildItem .\packages -Recurse -Include '*.yaml' | % {
         (Join-Path -Path $_.Directoryname -ChildPath ( $inhalt["PackageIdentifier"]+".def.yaml"))
     )
 
-    $cpyMerge = ""
-
-
-
     $combInh = @{}
     # Schleife zum Durchlaufen der YAML-Dateien
     foreach ($datei in $quelleDateien) {
 
         if (Test-Path $datei) {
 
-
-            $inhalt = Get-Content -Path $datei -Raw
-            #$cpyMerge += $inhalt
-            #$yamlInhalt = $inhalt | ConvertFrom-Yaml #$inhalt | ConvertFrom-Yaml
-            # foreach ($key in $yamlInhalt.Keys) {
-            #     $combInh[$key] = $yamlInhalt[$key]
-            # }
-            # if($yamlInhalt.Keys -contains "ManifestType"){
-            #     # $yamlInhalt["ManifestType"] = "merged"
-            #     $yamlInhalt.Remove("ManifestType")
-            # }
-
-            $cpyMerge += $inhalt #($yamlInhalt | ConvertTo-Yaml)
-
-
-            $cpyMerge
-
+            $inhalt = Get-Content -Path $datei
+            $yamlInhalt = $inhalt | ConvertFrom-Yaml
+            foreach ($key in $yamlInhalt.Keys) {
+                $combInh[$key] = $yamlInhalt[$key]
+            }
         }   
     }
 
-    # $combInh["ManifestType"] = "merged"
+    $combInh["ManifestType"] = "merged"
 
     $zielDatei = (Join-Path -Path $_.Directoryname -ChildPath ($pkgid+".def.yaml"))
 
-    Set-Content -Path $zielDatei -Value $cpyMerge #($combInh | ConvertTo-Yaml)
+    Set-Content -Path $zielDatei -Value ($combInh | ConvertTo-Yaml)
 
     Write-Host "Die kombinierte Datei wurde erstellt: $zielDatei"
 }
