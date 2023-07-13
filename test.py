f7829282541540ea9783a67c0ec371ab4f8377df
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-13 09:08:32 +0200

    echo added

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ import json
 # post_response_json = post_response.json()
 # print(post_response_json)
 
-url_post = "http://101.188.67.134:1337/api/approved-packages"
+url_post = "https://b171-92-63-213-202.ngrok-free.app/api/approved-packages"
 
 token = "bearer ee3d6da7cf68479d1392d3a9fd45282ec14499a8832556465dd236504582a834842a1025626441d9c84a49265a1c75fdde2936c5f6b03f1c2339b39351a26ecea7c07735f522374b4a36a7169e4691bc586777f0800db321a8ba3abe018e6d80de0810001f5805630dc3494d6bb77cf96e6668f46830b3787a4cf0687eaede64"
 
@@ -36,7 +36,8 @@ updated_package = {
 payload = {
     "data": updated_package
 }
-post_response = requests.get(url=url_post, json=payload, headers={"Authorization": token, "Content-Type": "application/json"})
+post_response = requests.post(url=url_post, json=payload, headers={"Authorization": token, "Content-Type": "application/json"})
 # Print the response
+print(post_response)
 post_response_json = post_response.json()
 print(post_response_json)
