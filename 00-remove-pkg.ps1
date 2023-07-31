fb91c0e77a53a046d1a861eef413fe3dc38b4549
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-31 11:22:52 +0200

    remove improved

--- 00-remove-pkg.ps1
+++ 00-remove-pkg.ps1
@@ -2,13 +2,60 @@ param (
     $url
 )
 
+$parts = $url.Split("/")[0..($url.Split("/").Length-3)]
 
-# $parts = $url.Split("/")[-5..-1]
+$folderPath = $parts -join "/"
 
-# $filePath = $parts -join "/"
+$folderPath = "packages/" + $folderPath
 
-$filePath = "packages/" + $url
+$subfolders = Get-ChildItem -Path $folderPath -Directory
 
-if (Test-Path $filePath) {
-    Remove-Item -Path $filePath
+$numSubfolders = $subfolders.Count
+
+$numSubfolders
+
+
+if($numSubfolders -gt 1){
+
+    #Delete specific version-folder
+
+    $parts = $url.Split("/")[0..($url.Split("/").Length-2)]
+
+    $folderPath = $parts -join "/"
+
+    $filePath = "packages/" + $folderPath
+
+    if (Test-Path $filePath) {
+        Remove-Item -Path $filePath -Recurse
+    }
+}else{
+
+    #delete whole package
+
+    if (Test-Path $folderPath) {
+        Remove-Item -Path $folderPath -Recurse
+    }
+
+
+    $parts = $url.Split("/")[0..($url.Split("/").Length-4)]
+
+    $folderPath = $parts -join "/"
+
+    $folderPath = "packages/" + $folderPath
+
+    $subfolders = Get-ChildItem -Path $folderPath -Directory
+
+    $numSubfolders = $subfolders.Count
+
+    #check whether publisher has other packages
+    if($numSubfolders -eq 0){
+        #remove publisher-folder
+        if (Test-Path $folderPath) {
+            Remove-Item -Path $folderPath -Recurse
+        }
+    }
+
 }
+
+
+
