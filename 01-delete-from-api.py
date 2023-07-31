f76c3f925de366d930c972919a9d4aa4d691abf8
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-31 14:48:18 +0200

    identifierArray added

--- 01-delete-from-api.py
+++ 01-delete-from-api.py
@@ -1,21 +1,21 @@
 import os
 import requests
+import sys
 
 # url = "https://af4b-92-63-213-202.ngrok-free.app/api/approved-packages"
 url = os.environ.get("API_URl")+"approved-packages"
-
-identifier = os.environ.get("PKG_IDENTIFIER")
-
 token = os.environ.get("API_TOKEN")
 
-version = url.split("/")[-2]
+identifier = sys.argv[1]
 
-print(version)
+if(identifier):
 
-fetchResponse = requests.get(url=(url+'?filters[identifier][$eq]='+identifier), headers={"Authorization": token, "Content-Type": "application/json"})
-fetchResponseJson = fetchResponse.json()
-print(fetchResponseJson)
-if(len(fetchResponseJson["data"])>0):
-    id = fetchResponseJson["data"][0]["id"]
-    delresp = requests.delete(url=(url+"/"+str(id)), headers={"Authorization": "token", "Content-Type": "application/json"})
-    print(delresp)
+    fetchResponse = requests.get(url=(url+'?filters[identifier][$eq]='+identifier), headers={"Authorization": token, "Content-Type": "application/json"})
+    fetchResponseJson = fetchResponse.json()
+    if(len(fetchResponseJson["data"])>0):
+        id = fetchResponseJson["data"][0]["id"]
+        delresp = requests.delete(url=(url+"/"+str(id)), headers={"Authorization": token, "Content-Type": "application/json"})
+        print(delresp)
+
+
+
