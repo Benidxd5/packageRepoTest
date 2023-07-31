bdc242b1fe286819be5d16a240bfc69d3c494c00
Benjamin Terbul <terbulb@edu.htl-villach.at>
2023-07-31 09:36:47 +0200

    commit

--- wingetFetch.py
+++ wingetFetch.py
@@ -6,13 +6,14 @@ import math
 
 db_path = ".tmp/source/Public/index.db"
 
-token = os.environ.get("API_TOKEN")
+# token = os.environ.get("API_TOKEN")
 
-url_post = os.environ.get("API_URl")+"packages"
+# url_post = os.environ.get("API_URl")+"packages"
 
-# url_post = "http://10.10.10.138:1337/api/packages"
+url_post = "http://localhost:1337/api/packages"
 # token = "bearer d547a841dc8b98d9234e94238d09aa4aad78b985d294d5d45feca6ebdb16d0dcaee0c3bd4e357e02998d5dd4e45090a02323dbb4ffb3ee083ac824e6db67792a9a9713454e3186e9ea18172ca3730e42dfdcc06c3a7b34fd1a40afc1f699a0d6619e1920ba20358d7281cf0b6984b6b76a9f01be1873ab71031c61a78855e11b"
 
+token = "bearer 65f36576725143d11424c122a34d3884e5129dccb1abc126438191d2c3fafbe04568a1e58db072d0b205e6eec77360ab7d2b115796ea5e271ff75e81094747386afd5875e42bc806c611a751586b619e1f6c914247dc162fdd6d34c39c1d8af4bd3da0096dbcbfce9085ee0b93899af4e8aa839da4f59e4a8782aa4a5f8cdb31"
 
 packageVersions = {}
 
@@ -109,8 +110,9 @@ progress[:math.floor(1/numPkgs*100)] = ["|"]*(math.floor(1/numPkgs*100))
 for idx in row:
     pkgData = createPackageDataObj(idx,cursor)
     pushPackage(pkgData)
-    progress[math.floor(pkgNum/numPkgs*100)] = "|"
-    print("".join(progress))
+    #progress[math.floor(pkgNum/numPkgs*100)] = "|"
+    #print("".join(progress))
+    print(pkgNum)
     pkgNum+=1
 
 
